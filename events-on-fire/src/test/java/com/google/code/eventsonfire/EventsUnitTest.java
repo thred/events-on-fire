7c2108a193ca48b5ac7a56a007b22295475561f6
manfred.hantschel <manfred.hantschel@gmail.com>
2012-03-02 18:57:32 +0000

    added ability to bind/unbind to classes

--- events-on-fire/src/test/java/com/google/code/eventsonfire/EventsUnitTest.java
+++ events-on-fire/src/test/java/com/google/code/eventsonfire/EventsUnitTest.java
@@ -22,24 +22,20 @@ package com.google.code.eventsonfire;
 import org.testng.annotations.Test;
 
 @Test
-public class EventsUnitTest
-{
+public class EventsUnitTest {
 
 	@Test(expectedExceptions = IllegalArgumentException.class)
-	public void testBindWithoutProducer()
-	{
+	public void testBindWithoutProducer() {
 		Events.bind(null, new EventsTestConsumer());
 	}
 
 	@Test(expectedExceptions = IllegalArgumentException.class)
-	public void testBindWithoutConsumer()
-	{
+	public void testBindWithoutConsumer() {
 		Events.bind(new Object(), null);
 	}
 
 	@Test
-	public void testFire() throws InterruptedException
-	{
+	public void testFire() throws InterruptedException {
 		final Object producer = new Object();
 		final EventsTestConsumer consumerA = new EventsTestConsumer();
 		final EventsTestConsumer consumerB = new EventsTestConsumer();
@@ -69,12 +65,76 @@ public class EventsUnitTest
 		assert consumerB.size() == 0;
 
 		assert "Event #3".equals(consumerA.popEvent().getEvent());
-
 	}
 
 	@Test
-	public void testDisable() throws InterruptedException
-	{
+	public void testFireToInstancesOf() throws InterruptedException {
+		Integer producer = new Integer(0);
+		EventsTestConsumer consumerOnObject = new EventsTestConsumer();
+		EventsTestConsumer consumerOnNumber = new EventsTestConsumer();
+		EventsTestConsumer consumerOnInteger = new EventsTestConsumer();
+		EventsTestConsumer consumerOnLong = new EventsTestConsumer();
+
+		Events.bind(consumerOnObject);
+		Events.bind(Number.class, consumerOnNumber);
+		Events.bind(Integer.class, consumerOnInteger);
+		Events.bind(Long.class, consumerOnLong);
+
+		Events.fire(producer, "Event #1");
+
+		consumerOnObject.waitForSize(1);
+		consumerOnNumber.waitForSize(1);
+		consumerOnInteger.waitForSize(1);
+		assert consumerOnLong.size() == 0;
+
+		assert "Event #1".equals(consumerOnObject.popEvent().getEvent());
+		assert "Event #1".equals(consumerOnNumber.popEvent().getEvent());
+		assert "Event #1".equals(consumerOnInteger.popEvent().getEvent());
+
+		Events.unbind(consumerOnNumber);
+		Events.unbind(Number.class, consumerOnInteger);
+		Events.unbind(Number.class, consumerOnLong);
+
+		Events.fire(producer, "Event #2");
+
+		consumerOnObject.waitForSize(1);
+		assert consumerOnNumber.size() == 0;
+		assert consumerOnInteger.size() == 0;
+		assert consumerOnLong.size() == 0;
+
+		assert "Event #2".equals(consumerOnObject.popEvent().getEvent());
+	}
+
+	@Test
+	public void testFireToInstancesOfSpecialA() throws InterruptedException {
+		Integer producer = new Integer(0);
+		EventsTestConsumer consumer = new EventsTestConsumer();
+
+		Events.bind(Object.class, consumer);
+		Events.bind(Number.class, consumer);
+		Events.bind(producer, consumer);
+		Events.fire(producer, "Event #1");
+		
+		consumer.waitForSize(3);
+
+		assert "Event #1".equals(consumer.popEvent().getEvent());
+		assert "Event #1".equals(consumer.popEvent().getEvent());
+		assert "Event #1".equals(consumer.popEvent().getEvent());
+
+		Events.unbind(Integer.class, consumer);
+		Events.fire(producer, "Event #1");
+		
+		consumer.waitForSize(2);
+
+		assert "Event #1".equals(consumer.popEvent().getEvent());
+		assert "Event #1".equals(consumer.popEvent().getEvent());
+
+		Events.unbind(Object.class, consumer);
+		assert consumer.size() == 0;
+	}
+
+	@Test
+	public void testDisable() throws InterruptedException {
 		final Object producer = new Object();
 		final EventsTestConsumer consumer = new EventsTestConsumer();
 
@@ -93,13 +153,12 @@ public class EventsUnitTest
 		assert "Event #1".equals(consumer.popEvent().getEvent());
 	}
 
-	public void testCleanupConsumer()
-	{
-		//		final Object producer = new Object();
-		//		final TestConsumer consumer = new TestConsumer();
-		//		
-		//		Events.bind(producer, consumer);
+	public void testCleanupConsumer() {
+		// final Object producer = new Object();
+		// final TestConsumer consumer = new TestConsumer();
+		//
+        // Events.bind(producer, consumer);
 
-	}
+    }
 
 }
