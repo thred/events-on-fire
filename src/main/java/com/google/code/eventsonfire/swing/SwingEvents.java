c5ae27781b7bd39f5be8b0ecdd19f2e557fc170e
manfred.hantschel <manfred.hantschel@gmail.com>
2012-03-21 19:47:49 +0000

    Added fireOnKeyStroke methods to SwingEvents

--- src/main/java/com/google/code/eventsonfire/swing/SwingEvents.java
+++ src/main/java/com/google/code/eventsonfire/swing/SwingEvents.java
@@ -1,5 +1,10 @@
 package com.google.code.eventsonfire.swing;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ListSelectionEvent;
@@ -44,8 +49,8 @@ public class SwingEvents extends AWTEvents
     }
 
     /**
-     * A custom implementation of the list selection listener interface using either the specified producer or the source of the
-     * event.
+     * A custom implementation of the list selection listener interface using either the specified producer or the
+     * source of the event.
      * 
      * @author ham
      */
@@ -53,8 +58,8 @@ public class SwingEvents extends AWTEvents
     {
 
         /**
-         * Creates a list selection listener using the specified producer and tags. If the producer is null, the source of the
-         * event will be used as producer.
+         * Creates a list selection listener using the specified producer and tags. If the producer is null, the source
+         * of the event will be used as producer.
          * 
          * @param producer the producer, may be null
          * @param tags the tags, may be null
@@ -74,8 +79,8 @@ public class SwingEvents extends AWTEvents
     }
 
     /**
-     * A custom implementation of the tree selection listener interface using either the specified producer or the source of the
-     * event.
+     * A custom implementation of the tree selection listener interface using either the specified producer or the
+     * source of the event.
      * 
      * @author ham
      */
@@ -83,8 +88,8 @@ public class SwingEvents extends AWTEvents
     {
 
         /**
-         * Creates a tree selection listener using the specified producer and tags. If the producer is null, the source of the
-         * event will be used as producer.
+         * Creates a tree selection listener using the specified producer and tags. If the producer is null, the source
+         * of the event will be used as producer.
          * 
          * @param producer the producer, may be null
          * @param tags the tags, may be null
@@ -107,9 +112,121 @@ public class SwingEvents extends AWTEvents
     private static final ListSelectionListener DEFAULT_LIST_SELECTION_LISTENER = new EventsListSelectionListener(null);
     private static final TreeSelectionListener DEFAULT_TREE_SELECTION_LISTENER = new EventsTreeSelectionListener(null);
 
+    private static long uniqueKey = 0;
+
     static
     {
         Events.registerStrategy(new SwingEventHandlerAnnotationStrategy());
+    }
+
+    /**
+     * Registers a keystroke and an action to the component. Fires an action event, if the component that receives the
+     * keystroke is member of a focused window.
+     * 
+     * @param producer the component
+     * @param keyStroke the keystroke
+     * @param tags some tags
+     * @return the component
+     */
+    public static <TYPE extends JComponent> TYPE fireOnWindowsKeyStroke(final TYPE producer, String keyStroke,
+        final String... tags)
+    {
+        return fireOnWindowsKeyStroke(producer, KeyStroke.getKeyStroke(keyStroke), tags);
+    }
+
+    /**
+     * Registers a keystroke and an action to the component. Fires an action event, if the component that receives the
+     * keystroke is member of a focused window.
+     * 
+     * @param producer the component
+     * @param keyStroke the keystroke
+     * @param tags some tags
+     * @return the component
+     */
+    public static <TYPE extends JComponent> TYPE fireOnWindowsKeyStroke(final TYPE producer, KeyStroke keyStroke,
+        final String... tags)
+    {
+        return fireOnKeyStroke(producer, JComponent.WHEN_IN_FOCUSED_WINDOW, keyStroke, tags);
+    }
+
+    /**
+     * Registers a keystroke and an action to the component. Fires an action event, if an ancestor of the specified
+     * component receives the keystroke.
+     * 
+     * @param producer the component
+     * @param keyStroke the keystroke
+     * @param tags some tags
+     * @return the component
+     */
+    public static <TYPE extends JComponent> TYPE fireOnAncestorsKeyStroke(final TYPE producer, String keyStroke,
+        final String... tags)
+    {
+        return fireOnAncestorsKeyStroke(producer, KeyStroke.getKeyStroke(keyStroke), tags);
+    }
+
+    /**
+     * Registers a keystroke and an action to the component. Fires an action event, if an ancestor of the specified
+     * component receives the keystroke.
+     * 
+     * @param producer the component
+     * @param keyStroke the keystroke
+     * @param tags some tags
+     * @return the component
+     */
+    public static <TYPE extends JComponent> TYPE fireOnAncestorsKeyStroke(final TYPE producer, KeyStroke keyStroke,
+        final String... tags)
+    {
+        return fireOnKeyStroke(producer, JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT, keyStroke, tags);
+    }
+
+    /**
+     * Registers a keystroke and an action to the component. Fires an action event, if the component is focused and
+     * receives the keystroke.
+     * 
+     * @param producer the component
+     * @param keyStroke the keystroke
+     * @param tags some tags
+     * @return the component
+     */
+    public static <TYPE extends JComponent> TYPE fireOnKeyStroke(final TYPE producer, String keyStroke,
+        final String... tags)
+    {
+        return fireOnKeyStroke(producer, KeyStroke.getKeyStroke(keyStroke), tags);
+    }
+
+    /**
+     * Registers a keystroke and an action to the component. Fires an action event, if the component is focused and
+     * receives the keystroke.
+     * 
+     * @param producer the component
+     * @param keyStroke the keystroke
+     * @param tags some tags
+     * @return the component
+     */
+    public static <TYPE extends JComponent> TYPE fireOnKeyStroke(final TYPE producer, KeyStroke keyStroke,
+        final String... tags)
+    {
+        return fireOnKeyStroke(producer, JComponent.WHEN_FOCUSED, keyStroke, tags);
+    }
+
+    private static <TYPE extends JComponent> TYPE fireOnKeyStroke(final TYPE producer, int condition,
+        KeyStroke keyStroke, final String... tags)
+    {
+        String actionMapKey = "Events Key #" + uniqueKey++;
+
+        producer.getInputMap(condition).put(keyStroke, actionMapKey);
+        producer.getActionMap().put(actionMapKey, new AbstractAction()
+        {
+            private static final long serialVersionUID = 5541145536773299681L;
+
+            public void actionPerformed(ActionEvent e)
+            {
+                Events.fire(producer, e,
+                    ((tags != null) && (tags.length > 0)) ? tags : new String[]{e.getActionCommand()});
+            }
+        });
+
+        return producer;
     }
 
     /**
@@ -146,8 +263,8 @@ public class SwingEvents extends AWTEvents
     }
 
     /**
-     * Creates a list selection listener, that fires an event using the specified producer and the specified tags. If the
-     * producer is null, the source of the event will be used as producer.
+     * Creates a list selection listener, that fires an event using the specified producer and the specified tags. If
+     * the producer is null, the source of the event will be used as producer.
      * 
      * @param producer the producer
      * @param tags the tags
@@ -169,8 +286,8 @@ public class SwingEvents extends AWTEvents
     }
 
     /**
-     * Creates a tree selection listener, that fires an event using the specified producer and the specified tags. If the
-     * producer is null, the source of the event will be used as producer.
+     * Creates a tree selection listener, that fires an event using the specified producer and the specified tags. If
+     * the producer is null, the source of the event will be used as producer.
      * 
      * @param producer the producer
      * @param tags the tags
