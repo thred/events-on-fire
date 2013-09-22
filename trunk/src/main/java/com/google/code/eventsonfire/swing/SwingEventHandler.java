d11866d466027e22c7d9073f2d707bf8c39d4517
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com@96312e71-6469-176c-3b34-357b6ad6946e>
2013-09-22 05:06:31 +0000

    updated copyright notice

--- trunk/src/main/java/com/google/code/eventsonfire/swing/SwingEventHandler.java
+++ trunk/src/main/java/com/google/code/eventsonfire/swing/SwingEventHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2012 events-on-fire Team This file is part of Events-On-Fire
+ * Copyright (c) 2011-2013 events-on-fire Team This file is part of Events-On-Fire
  * (http://code.google.com/p/events-on-fire), licensed under the terms of the MIT License (MIT). Permission is hereby
  * granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the
  * "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
